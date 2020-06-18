b25b172132321f68ea91bfffa570ddd0e78b28f6
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-18 23:52:51 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World Mr. Software Engineer Muhammad Ismail 
+          Hello World Mr.Muhammad Ismail 
         </p>
         <a
           className="App-link"
